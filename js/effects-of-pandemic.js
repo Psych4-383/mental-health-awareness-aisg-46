3343dc219c96a5953fa205ea361ba96d7d78b280
Psych4-383 <pikachu.bisht.67@gmail.com>
2021-08-15 16:14:30 +0530

    edited

--- js/effects-of-pandemic.js
+++ js/effects-of-pandemic.js
@@ -1,1 +1,16 @@
-const img_1 = document.getElementById('i-1');
+const img_1 = Array.from(document.getElementsByTagName('img'));
+console.log(img_1)
+img_1.forEach(tag=> {
+    tag.classList.remove('img-transition');
+})
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            img_1.classList.add('img-transition');
+            return;
+        }
+        img_1.classList.remove('img-transition')
+    })
+})
+
+observer.observe(document.querySelector('.imgdiv'))
