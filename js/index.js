55c2b190ac1a01efb090baf5ef380f9ce44f3ed6
Psych4-383 <pikachu.bisht.67@gmail.com>
2021-08-15 14:38:05 +0530

    imgs added

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 const navUl = document.getElementById('nav-ul');
 const navLiS = Array.from(document.getElementsByClassName('nav-li'));
 const nav = document.getElementsByTagName('nav')[0];
+const button_nav = document.getElementById('nav-hamburger');
 navMinimise()
 var navFull = false;
 
@@ -11,6 +12,7 @@ function navMinimise() {
     navUl.style.width = 'fit-content';
     nav.style.width = 'fit-content';
     navFull = false;
+    button_nav.innerHTML = '&#9776;'
 }
 function navShow() {
     if (navFull === true){
@@ -22,5 +24,6 @@ function navShow() {
         navUl.style.width = '100%';
         nav.style.width = '20%'
         navFull = true;
+        button_nav.innerHTML = '&#x2716;'
     }
 }
